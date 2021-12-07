186a76390c6b5281b99a3ea9e531239ec2bd08f4
CauchyPeano <431522+CauchyPeano@users.noreply.github.com>
2021-12-07 23:39:49 +0100

    day 3 done

--- src/day3/Day3.kt
+++ src/day3/Day3.kt
@@ -8,19 +8,62 @@ fun main() {
     problem2()
 }
 
+private const val dim = 12
+
 fun problem2() {
     val ints = File("src/day3/input3.txt")
         .readLines()
         .map { Integer.parseInt(it, 2) }
 
+    val oxyNum = getOxyNum(ints)
+    println("oxy: " + oxyNum.toString(2))
+
+    val co2Num = getCo2Num(ints)
+    println("co2: " + co2Num.toString(2))
+
+    println("Problem2: ${co2Num * oxyNum}")
 }
+
+private fun getOxyNum(ints: List<Int>): Int {
+    var oxy = ints.filter { isOxy(it) }
+    for (i in (dim - 2) downTo 0) {
+        if (oxy.count() == 1) return oxy.first()
+
+        val part = oxy.partition { ((1 shl i) and it) > 0 }
+
+        if (part.first.count() >= part.second.count()) {
+            oxy = part.first
+        } else {
+            oxy = part.second
+        }
+    }
+    return oxy.first()
+}
+
+private fun getCo2Num(ints: List<Int>): Int {
+    var co2 = ints.filter { !isOxy(it) }
+    for (i in (dim - 2) downTo 0) {
+        if (co2.count() == 1) return co2.first()
+
+        val part = co2.partition { ((1 shl i) and it) > 0 }
+
+        if (part.first.count() < part.second.count()) {
+            co2 = part.first
+        } else {
+            co2 = part.second
+        }
+    }
+    return co2.first()
+}
+
+private fun isOxy(it: Int) = (1 shl (dim - 1)) and it > 0
 
 fun problem1() {
     val ints = File("src/day3/input3.txt")
         .readLines()
         .map { Integer.parseInt(it, 2) }
 
-    val arr = IntArray(12)
+    val arr = IntArray(dim)
 
     for (num in ints) {
         for ((idx, _) in arr.withIndex()) {
